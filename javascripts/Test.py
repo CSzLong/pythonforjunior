f538f50399ac2f945212fe37b0184a87fc647bf9
孙老师-Chris <szlong1987@gmail.com>
2024-07-11 10:53:48 +0800

    mirror change

--- javascripts/Test.py
+++ javascripts/Test.py
@@ -1,23 +1,107 @@
-"""
-打印出1000-9999之间的所有四叶草数:
-即 千位四次方+百位四次方+十位四次方+个位四次方等于这个数
+import tkinter as tk
+from tkinter import filedialog, messagebox
 
-"""
+class TextEditor:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Untitled - Simple Text Editor")
+        self.root.geometry("800x600")
 
-for i in range(1000, 10000):
-    s = str(i)
-    qw, bw, sw, gw = int(s[0]), int(s[1]), int(s[2]), int(s[3])
-    if i == qw ** 4 + bw ** 4 + sw ** 4 + gw ** 4:
-        print(i)
+        self.current_file = None
+        self.text_area = tk.Text(self.root, undo=True)
+        self.text_area.pack(fill=tk.BOTH, expand=1)
+        self.text_area.bind("<<Modified>>", self.on_modified)
 
-"""
-判断一个数是不是回文数，回文数就是将这个数反过来之后，和原数是相等的
-12321是回文数
-12345不是回文数
-"""
+        self.menu_bar = tk.Menu(self.root)
+        self.root.config(menu=self.menu_bar)
 
-n = input('Input a number:')
-if n == n[::-1]:
-    print('Yes')
-else:
-    print('No')
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+        self.file_menu.add_command(label="New", command=self.new_file)
+        self.file_menu.add_command(label="Open", command=self.open_file)
+        self.file_menu.add_command(label="Save", command=self.save_file)
+        self.file_menu.add_command(label="Save As", command=self.save_as_file)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", command=self.exit_editor)
+
+        self.edit_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
+        self.edit_menu.add_command(label="Undo", command=self.text_area.edit_undo)
+        self.edit_menu.add_command(label="Redo", command=self.text_area.edit_redo)
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label="Cut", command=lambda: self.text_area.event_generate("<<Cut>>"))
+        self.edit_menu.add_command(label="Copy", command=lambda: self.text_area.event_generate("<<Copy>>"))
+        self.edit_menu.add_command(label="Paste", command=lambda: self.text_area.event_generate("<<Paste>>"))
+
+        self.help_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Help", menu=self.help_menu)
+        self.help_menu.add_command(label="About", command=self.show_about)
+
+        self.text_area.edit_modified(False)
+
+    def new_file(self):
+        if self.confirm_save_changes():
+            self.text_area.delete(1.0, tk.END)
+            self.current_file = None
+            self.root.title("Untitled - Simple Text Editor")
+
+    def open_file(self):
+        if self.confirm_save_changes():
+            file_path = filedialog.askopenfilename(defaultextension=".txt",
+                                                   filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+            if file_path:
+                with open(file_path, "r") as file:
+                    self.text_area.delete(1.0, tk.END)
+                    self.text_area.insert(tk.END, file.read())
+                self.current_file = file_path
+                self.root.title(f"{file_path} - Simple Text Editor")
+                self.text_area.edit_modified(False)
+
+    def save_file(self):
+        if self.current_file:
+            with open(self.current_file, "w") as file:
+                file.write(self.text_area.get(1.0, tk.END))
+            self.text_area.edit_modified(False)
+        else:
+            self.save_as_file()
+
+    def save_as_file(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt",
+                                                 filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+        if file_path:
+            with open(file_path, "w") as file:
+                file.write(self.text_area.get(1.0, tk.END))
+            self.current_file = file_path
+            self.root.title(f"{file_path} - Simple Text Editor")
+            self.text_area.edit_modified(False)
+
+    def confirm_save_changes(self):
+        if self.text_area.edit_modified():
+            response = messagebox.askyesnocancel("Save Changes",
+                                                 "Do you want to save changes to your document?")
+            if response:  # Yes
+                self.save_file()
+                return True
+            elif response is None:  # Cancel
+                return False
+        return True
+
+    def exit_editor(self):
+        if self.confirm_save_changes():
+            self.root.quit()
+
+    def show_about(self):
+        messagebox.showinfo("About", "Simple Text Editor\nBuilt with Python and Tkinter")
+
+    def on_modified(self, event):
+        if self.current_file:
+            self.root.title(f"*{self.current_file} - Simple Text Editor")
+        else:
+            self.root.title("*Untitled - Simple Text Editor")
+        self.text_area.edit_modified(False)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    editor = TextEditor(root)
+    root.protocol("WM_DELETE_WINDOW", editor.exit_editor)
+    root.mainloop()
